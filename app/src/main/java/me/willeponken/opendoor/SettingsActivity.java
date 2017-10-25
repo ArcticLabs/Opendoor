e5c5983961ed87981799499c3e77984fe66fe530
Edvin Sladic <sladic.edvin@gmail.com>
2017-10-25 16:15:55 +0200

    Fixed memory leak when navigating back to MainActivity

--- app/src/main/java/me/willeponken/opendoor/SettingsActivity.java
+++ app/src/main/java/me/willeponken/opendoor/SettingsActivity.java
@@ -131,7 +131,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), MainActivity.class));
+                getActivity().finish();
                 return true;
             }
             return super.onOptionsItemSelected(item);
